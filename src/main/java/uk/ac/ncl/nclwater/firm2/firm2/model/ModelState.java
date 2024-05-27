c663fe5364b9570984b991fc5f441d18416b68ad
Jannetta Steyn <6432530+jsteyn@users.noreply.github.com>
2024-05-27 20:23:29 +0100

    Add class to hold model state from timeline

---
+++ src/main/java/uk/ac/ncl/nclwater/firm2/firm2/model/ModelState.java
@@ -0,0 +1,6 @@
+package uk.ac.ncl.nclwater.firm2.firm2.model;
+
+public class ModelState {
+    String time;
+    Integer seaLevel;
+}
--- src/main/java/uk/ac/ncl/nclwater/firm2/firm2/model/ModelStateChanges.java
+++ src/main/java/uk/ac/ncl/nclwater/firm2/firm2/model/ModelStateChanges.java
@@ -2,6 +2,6 @@ package uk.ac.ncl.nclwater.firm2.firm2.model;
 
 import java.util.ArrayList;
 
-public class ModelStateChanges extends ArrayList<VehicleState> {
+public class ModelStateChanges extends ArrayList<ModelState> {
 
 }
