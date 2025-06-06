7b87f355b3767aeca352476a5a72eb4414c84077
Robin Wardle <robin.wardle@newcastle.ac.uk>
2025-06-06 11:41:07 +0100

    Updated model metadata and added AGPL licensing

--- src/main/java/uk/ac/ncl/nclwater/firm2/examples/BNGToLatLonConverter.java
+++ src/main/java/uk/ac/ncl/nclwater/firm2/examples/BNGToLatLonConverter.java
@@ -1,3 +1,23 @@
+/******************************************************************************
+ * Copyright 2025 Newcastle University
+ *
+ * This file is part of FIRM2.
+ *
+ * FIRM2 is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * FIRM2 is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with FIRM2. If not, see <https://www.gnu.org/licenses/>. 
+ *****************************************************************************/
+
+
 package uk.ac.ncl.nclwater.firm2.examples;
 
 import java.lang.Math;
